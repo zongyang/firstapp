c2aa5a4f81f48b177d6faf1a7b57d6578cacc451
zhangzengyang <zhangzengyang@aliyun.com>
2014-11-24 02:50:14 +0800

    fifth

---
+++ WebRoot/js/msg_function.js
@@ -0,0 +1,13 @@
+$(function(){
+	//left
+	$('.msg-main-l li').click(liClick);
+	//middle
+	$('.msg-main-m li').click(liClick);
+	//right
+})
+
+function liClick(){
+	var self=$(this);
+	self.addClass('active');
+	self.siblings().removeClass('active');
+}
